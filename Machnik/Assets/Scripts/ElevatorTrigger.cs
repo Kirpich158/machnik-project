fb24cbf4dd63469bdcf46bdb6d400c8b2ee9b95c
Kirill <kirillka.zub@gmail.com>
2022-01-17 01:05:34 +0300

    Project v1.0
    Finished full version 1.0 of out project

--- Machnik/Assets/Scripts/ElevatorTrigger.cs
+++ Machnik/Assets/Scripts/ElevatorTrigger.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 
 public class ElevatorTrigger : MonoBehaviour {
     [SerializeField] private GameObject elevator;
+    [SerializeField] private ParticleSystem particle;
 
     private void OnTriggerStay(Collider other) {
         if (other.tag == "Player" && Input.GetKeyDown(KeyCode.C)) {
             StartCoroutine(elevator.GetComponent<ElevatorScript>().Move(new Vector3(transform.position.x - 0.85f, transform.position.y + 17f, transform.position.z), 5f));
+            elevator.GetComponent<AudioSource>().Play();
+            particle.Play();
         }
 
         if (other.tag == "Player" && Input.GetKeyDown(KeyCode.V)) {
-            StartCoroutine(elevator.GetComponent<ElevatorScript>().Move(new Vector3(transform.position.x + 0.85f, transform.position.y - 17f, transform.position.z), 5f));
+            StartCoroutine(elevator.GetComponent<ElevatorScript>().Move(new Vector3(transform.position.x - 0.85f, transform.position.y - 13.2f, transform.position.z), 5f));
+            elevator.GetComponent<AudioSource>().Play();
+            particle.Play();
         }
     }
-
     // Доделать так, чтобы лифт оставался на втором этаже, а когда ты входишь в него снова, то ехал на низ
 }
